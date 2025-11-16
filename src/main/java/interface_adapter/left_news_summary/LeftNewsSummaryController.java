105d6e198a245c50d635ed18fb2fcbdaca66b441
Siya-G <siyagera91@gmail.com>
2025-11-15 22:32:36 -0500

    Implemented LeftNewsSummaryController.

--- src/main/java/interface_adapter/left_news_summary/LeftNewsSummaryController.java
+++ src/main/java/interface_adapter/left_news_summary/LeftNewsSummaryController.java
@@ -1,5 +1,19 @@
 package interface_adapter.left_news_summary;
 
+import use_case.left_news_summary.LeftNewsSummaryInputBoundary;
+import use_case.left_news_summary.LeftNewsSummaryInputData;
+
 public class LeftNewsSummaryController {
+
+    private final LeftNewsSummaryInputBoundary inputBoundary;
+
+    public LeftNewsSummaryController(LeftNewsSummaryInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void execute(String topic) {
+        LeftNewsSummaryInputData inputData = new LeftNewsSummaryInputData(topic);
+        inputBoundary.execute(inputData);
+    }
 }
 
