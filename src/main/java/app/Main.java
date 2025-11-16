0b031dd5fd791ce33d5198f8f9c0a9a6ed2444c3
Siya-G <siyagera91@gmail.com>
2025-11-16 02:58:01 -0500

    Created and implemented AppBuilder and LeftNewsSummarryView. Updated Main for left use case.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,18 @@
 package app;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        AppBuilder appBuilder = new AppBuilder();
+
+        JFrame application = appBuilder
+                .addLeftNewsSummaryView()
+                .addLeftNewsSummaryUseCase()
+                .build();
+
+        application.pack();
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
+    }
 }
