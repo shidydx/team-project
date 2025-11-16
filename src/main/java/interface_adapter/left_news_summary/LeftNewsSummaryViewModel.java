3201cdc612fe920a41d92da8ed6311f16f02f88b
Siya-G <siyagera91@gmail.com>
2025-11-15 21:16:15 -0500

    Implemented LeftNewsSummaryViewModel

--- src/main/java/interface_adapter/left_news_summary/LeftNewsSummaryViewModel.java
+++ src/main/java/interface_adapter/left_news_summary/LeftNewsSummaryViewModel.java
@@ -1,5 +1,18 @@
 package interface_adapter.left_news_summary;
 
 public class LeftNewsSummaryViewModel {
+    LeftNewsSummaryState state;
+
+    public LeftNewsSummaryViewModel() {
+        state = new LeftNewsSummaryState();
+    }
+
+    public LeftNewsSummaryState getState() {
+        return state;
+    }
+
+    public void setState(LeftNewsSummaryState state) {
+        this.state = state;
+    }
 }
 
