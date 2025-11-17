ebeff02f29f3305a4daf1e6cc50a6c99b8a64a3c
kainingwu <kainingwu.gz@gmail.com>
2025-11-17 18:26:46 -0500

    updated FetchNewsUseCase

--- src/main/java/use_case/enter_topic/FetchNewsUseCase.java
+++ src/main/java/use_case/enter_topic/FetchNewsUseCase.java
@@ -3,15 +3,17 @@ package use_case.enter_topic;
 import entity.Article;
 import use_case.NewsFetcherService;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
  * validate the keyword entered by the user and fetch relevant news articles
  */
 public class FetchNewsUseCase {
-    private NewsFetcherService newsFetcher;
+    private final NewsFetcherService newsFetcher;
 
+    public FetchNewsUseCase(NewsFetcherService newsFetcher) {
+        this.newsFetcher = newsFetcher;
+    }
     public boolean validateTopic (String keyword) {
         if (keyword.equals("")) {
             return false;
