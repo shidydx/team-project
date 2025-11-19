3272816e2e9c069b4ed1c79721136b2bb3eb05f3
kainingwu <kainingwu.gz@gmail.com>
2025-11-18 20:58:28 -0500

    Implemented EnterTopicUseCase

---
+++ src/main/java/use_case/enter_topic/EnterTopicOutputData.java
@@ -0,0 +1,12 @@
+package use_case.enter_topic;
+
+public class EnterTopicOutputData {
+    private String output;
+
+    public EnterTopicOutputData(String output) {
+        this.output = output;
+    }
+    public String getOutput() {
+        return output;
+    }
+}
